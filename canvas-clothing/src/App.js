b627fb5899405bfe184cad9dda2381d67067e299
Christian Naykene <younchriss@live.co.uk>
2022-05-23 23:11:41 +0100

    Initial commit

---
+++ canvas-clothing/src/App.js
@@ -0,0 +1,43 @@
+import './categories.styles.scss'
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Footwear'
+    },
+    {
+      id: 4,
+      title: 'Men'
+    },
+    {
+      id: 5,
+      title: 'Women'
+    }
+  ]
+
+  return (
+    <div className="categories-container">
+      {categories.map(({title, id}) => (
+        <div key={id} className="categories-container">
+          {/* img */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App;
